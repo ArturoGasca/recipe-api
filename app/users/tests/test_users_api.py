5f0f218e227add933838f7488ba7b4414d5fa2a4
Arturo Gasca <agasca1994@gmail.com>
2019-03-31 22:56:52 -0600

    Add endpoint for managing users

--- app/users/tests/test_users_api.py
+++ app/users/tests/test_users_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('users:create')
 LOGIN_URL = reverse('users:login')
+ME_URL = reverse('users:me')
 
 
 def create_user(**kwargs):
@@ -100,3 +101,55 @@ class PublicUsersApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', res.data)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUsersApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='agasca@gmail.com',
+            password='pass123',
+            name='name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST method is not allowed on the 'me' url"""
+        res = self.client.post(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {
+            'name': 'newname',
+            'email': 'newmail@gmail.com',
+            'password': 'newpass123'
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertEqual(self.user.email, payload['email'])
+        self.assertTrue(self.user.check_password(payload['password']))
--- app/users/urls.py
+++ app/users/urls.py
@@ -7,5 +7,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.CreateUserView.as_view(), name='create'),
-    path('login/', views.LoginView.as_view(), name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
 ]
